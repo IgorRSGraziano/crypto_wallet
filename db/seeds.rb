c85b1a893919174ed85040b6175fed188f11e0da
igorrsgraziano <igor.raphael@openk.com.br>
2022-10-15 14:14:41 -0300

    generate seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+coins = [{ description: 'Bitcoin', acronym: 'BTC', url_image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ethereum-icon-purple.svg/480px-Ethereum-icon-purple.svg.png' }, { description: 'Dash', acronym: 'DSH', url_image: 'https://media.dash.org/wp-content/uploads/dash-d.png' }, { description: 'Ethereum', acronym: 'ETH', url_image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ethereum-icon-purple.svg/480px-Ethereum-icon-purple.svg.png' }]
+
+miningtypes = [{ name: 'Proof Of Work', acronym: 'PoW' }, { name: 'Proof Of Stake', acronym: 'PoS' }]
+
+coins.each do |e|
+  Coin.find_or_create_by(e)
+end
+
+miningtypes.each do |e|
+  MiningType.find_or_create_by(e)
+end
