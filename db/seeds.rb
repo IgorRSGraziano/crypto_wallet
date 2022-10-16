59ce699a4505798ca980ec26922169b957376200
igorrsgraziano <igor.raphael@openk.com.br>
2022-10-16 17:05:03 -0300

    add relaction between mining types

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,19 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+%x(rails db:migrate)
 
-coins = [{ description: 'Bitcoin', acronym: 'BTC', url_image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ethereum-icon-purple.svg/480px-Ethereum-icon-purple.svg.png' }, { description: 'Dash', acronym: 'DSH', url_image: 'https://media.dash.org/wp-content/uploads/dash-d.png' }, { description: 'Ethereum', acronym: 'ETH', url_image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ethereum-icon-purple.svg/480px-Ethereum-icon-purple.svg.png' }]
+coins = [{ description: 'Bitcoin', acronym: 'BTC', url_image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ethereum-icon-purple.svg/480px-Ethereum-icon-purple.svg.png', mining_type_id: 1 },
+         { description: 'Dash', acronym: 'DSH', url_image: 'https://media.dash.org/wp-content/uploads/dash-d.png', mining_type_id: 1},
+         { description: 'Ethereum', acronym: 'ETH', url_image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ethereum-icon-purple.svg/480px-Ethereum-icon-purple.svg.png', mining_type_id: 1 }]
 
-miningtypes = [{ name: 'Proof Of Work', acronym: 'PoW' }, { name: 'Proof Of Stake', acronym: 'PoS' }]
+miningtypes = [{ description: 'Proof Of Work', acronym: 'PoW' }, { description: 'Proof Of Stake', acronym: 'PoS' }]
+
+miningtypes.each do |e|
+  MiningType.find_or_create_by(e)
+end
 
 coins.each do |e|
   Coin.find_or_create_by(e)
 end
 
-miningtypes.each do |e|
-  MiningType.find_or_create_by(e)
-end
